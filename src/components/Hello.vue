08ce74062a7fdca72ea270160d6a88a6311f048b
Johan Stenbeck <johan@colburn.se>
2019-11-20 09:08:38 +0000

    Update Hello.vue

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -16,7 +16,7 @@
               class
             >Matchplay är en matchspelstävling för par med officiellt handikapp. Par kan vara män, kvinnor eller mix. Tävlingen spelas i Sverige på golfklubbar anslutna till Svenska Golfförbundet.</p>
             
-             <b-alert show class="mt-4" variant="warning">
+             <b-alert show class="mt-4 small" variant="warning">
                 Anmälan för 2020 års tävling öppnar i mitten av december 2019, håll utkik här på sidan för mer info!
                 <!-- håll koll via <a href="https://www.facebook.com/pg/matchplaybusines" target="_blank">Facebook</a> och <a href="https://www.instagram.com/matchplaybusiness/" target="_blank">Instagram</a> -->
              </b-alert>
