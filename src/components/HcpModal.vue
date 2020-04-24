fc616de533a41a91a2cd7add65b5aa875491536a
Rasmus <rasmusjons@gmail.com>
2020-04-24 13:30:51 +0200

    Convert negative Hcp to positve through directive

--- src/components/HcpModal.vue
+++ src/components/HcpModal.vue
@@ -3,10 +3,18 @@
     <b-modal id="modal-1" title ok-only>
       <ul class="nav">
         <li class="nav-item">
-          <a class="nav-link active" @click="showData = true, showExplanation = false">Slope</a>
+          <a
+            class="nav-link active"
+            @click="(showData = true), (showExplanation = false)"
+            >Slope</a
+          >
         </li>
         <li class="nav-item">
-          <a class="nav-link" @click="showData = false, showExplanation = true">Förklaring</a>
+          <a
+            class="nav-link"
+            @click="(showData = false), (showExplanation = true)"
+            >Förklaring</a
+          >
         </li>
       </ul>
 
@@ -14,60 +22,75 @@
         <div v-if="showData">
           <b-row class="explanation">
             <b-col cols="4"></b-col>
-            <b-col cols="2" v-for="player in players" class="playerData">
-              <h5 v-initials>{{player.name}}</h5>
+            <b-col v-for="player in players" cols="2" class="playerData">
+              <h5 v-initials>{{ player.name }}</h5>
             </b-col>
           </b-row>
           <b-row>
             <b-col cols="4" class="explanation">
               <p>Exakt Hcp</p>
             </b-col>
-            <b-col cols="2" v-for="player in players" class="playerData">
-              <p>{{player.hcp}}</p>
+            <b-col v-for="player in players" cols="2" class="playerData">
+              <p v-negativeToPostive>{{ player.hcp }}</p>
             </b-col>
           </b-row>
           <b-row>
             <b-col cols="4" class="explanation">
               <p>Max 28 Hcp</p>
             </b-col>
-            <b-col cols="2" v-for="player in players" class="playerData">
-              <p>{{player.hcp > 28 ? 28 : player.hcp }}</p>
+            <b-col v-for="player in players" cols="2" class="playerData">
+              <p v-negativeToPostive>{{ player.hcp > 28 ? 28 : player.hcp }}</p>
             </b-col>
           </b-row>
           <b-row>
             <b-col cols="4" class="explanation">
               <p>Tee</p>
             </b-col>
-            <b-col cols="2" v-for="player in players" class="playerData">
-              <p>{{player.tee}}</p>
+            <b-col v-for="player in players" cols="2" class="playerData">
+              <p>{{ player.tee }}</p>
             </b-col>
           </b-row>
           <b-row>
             <b-col cols="4" class="explanation">
               <p>Slope Hcp</p>
             </b-col>
-            <b-col cols="2" v-for="hcpslope in slopeHandicapList" class="playerData">
-              <p>{{hcpslope > 28 ? 28 : hcpslope}}</p>
+            <b-col
+              v-for="hcpslope in slopeHandicapList"
+              cols="2"
+              class="playerData"
+            >
+              <p v-negativeToPostive>{{ hcpslope > 28 ? 28 : hcpslope }}</p>
             </b-col>
           </b-row>
           <b-row>
             <b-col cols="4" class="explanation">
               <p>10% reducering</p>
             </b-col>
-            <b-col cols="2" v-for="hcpslope in slopeHandicapList" class="playerData">
-              <p>{{hcpslope > 28 ? 28 * 0.9: hcpslope * 0.9}}</p>
+            <b-col
+              v-for="hcpslope in slopeHandicapList"
+              cols="2"
+              class="playerData"
+            >
+              <p v-negativeToPostive>
+                {{ hcpslope > 28 ? 28 * 0.9 : hcpslope * 0.9 }}
+              </p>
             </b-col>
           </b-row>
           <b-row>
             <b-col cols="4" class="explanation">
-              <p
-                v-if="slopeHandicapList[getIndexOfLowest] * 0.9 < 0"
-              >Lägg till {{slopeHandicapList[getIndexOfLowest] * 0.9 * -1}}, nolla lägsta hcp ({{slopeHandicapList[getIndexOfLowest] * 0.9 * -1}}) och avrunda till närmsta heltal</p>
-              <p
-                v-else
-              >Dra av {{slopeHandicapList[getIndexOfLowest] * 0.9}}, nolla lägsta hcp ({{slopeHandicapList[getIndexOfLowest] * 0.9}}) och avrunda till närmsta heltal</p>
+              <p v-if="slopeHandicapList[getIndexOfLowest] * 0.9 < 0">
+                Lägg till {{ slopeHandicapList[getIndexOfLowest] * 0.9 * -1 }},
+                nolla lägsta hcp ({{
+                  slopeHandicapList[getIndexOfLowest] * 0.9 * -1
+                }}) och avrunda till närmsta heltal
+              </p>
+              <p v-else>
+                Dra av {{ slopeHandicapList[getIndexOfLowest] * 0.9 }}, nolla
+                lägsta hcp ({{ slopeHandicapList[getIndexOfLowest] * 0.9 }}) och
+                avrunda till närmsta heltal
+              </p>
             </b-col>
-            <b-col cols="2" v-for="singleSlope in slope" class="playerData">
+            <b-col v-for="singleSlope in slope" cols="2" class="playerData">
               <p>{{ singleSlope }}</p>
             </b-col>
           </b-row>
@@ -75,13 +98,13 @@
             <b-col cols="4" class="explanation">
               <p>Nytt spel Hcp</p>
             </b-col>
-            <b-col cols="2" v-for="singleSlope in slope" class="playerData">
+            <b-col v-for="singleSlope in slope" cols="2" class="playerData">
               <p>{{ singleSlope }}</p>
             </b-col>
           </b-row>
         </div>
 
-        <div class="explanation" v-if="showExplanation">
+        <div v-if="showExplanation" class="explanation">
           <b-row>
             <b-col cols="6" class="explanation">
               <h5>Uträkning</h5>
@@ -92,63 +115,92 @@
             </b-col>
             <b-col cols="3" class="playerData">
               <h5>Exempel A</h5>
-              <p>HCP:{{exactHcpPlayerA}}</p>
-              <p>Course rating:{{courseRating}}</p>
-              <p>Slope rating:{{slopeRating}}</p>
-              <p>Banans rating:{{banansPar}}</p>
-              <strong>Slope {{slopeA}}</strong>
+              <p>HCP:{{ exactHcpPlayerA }}</p>
+              <p>Course rating:{{ courseRating }}</p>
+              <p>Slope rating:{{ slopeRating }}</p>
+              <p>Banans rating:{{ banansPar }}</p>
+              <strong>Slope {{ slopeA }}</strong>
             </b-col>
             <b-col cols="3" class="playerData">
               <h5>Exempel B</h5>
-              <p>HCP:{{exactHcpPlayerB}}</p>
-              <p>Course rating:{{courseRating}}</p>
-              <p>Slope rating:{{slopeRating}}</p>
-              <p>Banans rating:{{banansPar}}</p>
-              <strong>Slope {{slopeB}}</strong>
+              <p>HCP:{{ exactHcpPlayerB }}</p>
+              <p>Course rating:{{ courseRating }}</p>
+              <p>Slope rating:{{ slopeRating }}</p>
+              <p>Banans rating:{{ banansPar }}</p>
+              <strong>Slope {{ slopeB }}</strong>
             </b-col>
           </b-row>
           <b-row>
             <b-col cols="6" class="explanation">
-              <p>Efter att slopen {{slope}} räknats in reduceras respektive spelares HCP med 10%</p>
+              <p>
+                Efter att slopen {{ slope }} räknats in reduceras respektive
+                spelares HCP med 10%
+              </p>
             </b-col>
             <b-col cols="3" class="playerData">
-              <p>{{slopeA}} - 10% =</p>
-              <strong>{{ slopeA * 0.9}}</strong>
+              <p>{{ slopeA }} - 10% =</p>
+              <strong>{{ slopeA * 0.9 }}</strong>
             </b-col>
             <b-col cols="3" class="playerData">
-              <p>{{slopeB}} - 10% =</p>
-              <strong>{{ slopeB * 0.9}}</strong>
+              <p>{{ slopeB }} - 10% =</p>
+              <strong>{{ slopeB * 0.9 }}</strong>
             </b-col>
           </b-row>
           <b-row>
             <b-col cols="6" class="explanation">
-              <p>De tre spelarna med högst reducerat spelhandicap får i matchen slag motsvarande skillnaden till den spelaren som har lägst spelhandicap. Avrundning till närmaste heltal (uppåt vid 0.5, 1.5, 2.5 osv). Spelaren med lägst reducerat spelhandicap spelar med 0 i spelhandicap.</p>
+              <p>
+                De tre spelarna med högst reducerat spelhandicap får i matchen
+                slag motsvarande skillnaden till den spelaren som har lägst
+                spelhandicap. Avrundning till närmaste heltal (uppåt vid 0.5,
+                1.5, 2.5 osv). Spelaren med lägst reducerat spelhandicap spelar
+                med 0 i spelhandicap.
+              </p>
             </b-col>
             <b-col cols="3" class="playerData">
-              <p>{{ slopeA * 0.9}} - 1.8 =</p>
-              <strong>{{ Math.round(slopeA - (slopeA*0.1) - 1.8)}}</strong>
+              <p>{{ slopeA * 0.9 }} - 1.8 =</p>
+              <strong>{{ Math.round(slopeA - slopeA * 0.1 - 1.8) }}</strong>
             </b-col>
             <b-col cols="3" class="playerData">
-              <p>{{ slopeB * 0.9}} - 1.8 =</p>
+              <p>{{ slopeB * 0.9 }} - 1.8 =</p>
               <strong>0</strong>
             </b-col>
           </b-row>
         </div>
       </b-container>
     </b-modal>
-    <button class="btn btn-primary" v-b-modal.modal-1>Hur räknas slope ut?</button>
+    <button v-b-modal.modal-1 class="btn btn-primary">
+      Hur räknas slope ut?
+    </button>
   </div>
 </template>
 
 <script>
 export default {
+  directives: {
+    initials(el) {
+      //Om namnet eller initialerna är tre tecken långt tillsammans, så görs inga initialer.
+      if (el.innerText.length === 3) {
+        return;
+      }
+      let array = el.innerText.split(" ");
+      const intialsArray = array.map((e) => e.slice(0, 1));
+      el.innerHTML = intialsArray[0] + "." + intialsArray[1];
+    },
+    negativeToPostive(el) {
+      let number = parseInt(el.innerHTML);
+      if (number > 0) {
+        return;
+      }
+      el.innerHTML = "+" + number * -1;
+    },
+  },
   props: [
     "courseRating",
     "slopeRating",
     "banansPar",
     "players",
     "slope",
-    "slopeHandicapList"
+    "slopeHandicapList",
   ],
   data() {
     return {
@@ -156,7 +208,7 @@ export default {
       exactHcpPlayerB: 1.8,
       tee: 57,
       showExplanation: false,
-      showData: true
+      showData: true,
     };
   },
   method: {},
@@ -175,26 +227,15 @@ export default {
     },
     getIndexOfLowest() {
       let smallestHCP = 999;
-      this.slope.forEach(hcp => {
+      this.slope.forEach((hcp) => {
         if (hcp < smallestHCP) {
           smallestHCP = hcp;
         }
       });
-      const index = this.slope.findIndex(hcp => hcp === smallestHCP);
+      const index = this.slope.findIndex((hcp) => hcp === smallestHCP);
       return index;
-    }
+    },
   },
-  directives: {
-    initials(el) {
-      //Om namnet eller initialerna är tre tecken långt tillsammans, så görs inga initialer.
-      if (el.innerText.length === 3) {
-        return;
-      }
-      let array = el.innerText.split(" ");
-      const intialsArray = array.map(e => e.slice(0, 1));
-      el.innerHTML = intialsArray[0] + "." + intialsArray[1];
-    }
-  }
 };
 </script>
 
