fe34120e7d14569c77122b1764a24d75da69fa1b
Rasmus <rasmusjons@gmail.com>
2020-05-20 09:29:55 +0200

    Refactor helper.js

--- src/helpers.js
+++ src/helpers.js
@@ -27,45 +27,81 @@ const schp = (slopeRating, courseRating, banansPar, players) => {
 
   //FIXAR LAG hcp PÅ MAX 28
   //SPELARE 1-2
-  if (testData[0].hcp + testData[1].hcp > 28) {
-    if (testData[0].hcp === testData[1].hcp) {
-      const difference = testData[0].hcp + testData[1].hcp - 28;
-      testData[0].hcp = testData[0].hcp - difference / 2;
-      testData[1].hcp = testData[1].hcp - difference / 2;
+
+  function max28perTeam(hcp1, hcp2) {
+    let newHcp1 = hcp1;
+    let newHcp2 = hcp2;
+
+    if (hcp1 + hcp2 < 28) {
+      return;
+    }
+    if (hcp1 === hcp2) {
+      const difference = hcp1 + hcp2 - 28;
+      newHcp1 = hcp1 - difference / 2;
+      newHcp2 = hcp2 - difference / 2;
     }
 
-    const highestHCP = Math.max(testData[0].hcp, testData[1].hcp);
-    const substract = testData[0].hcp + testData[1].hcp - 28;
+    const highestHCP = Math.max(hcp1, hcp2);
+    const substract = hcp1 + hcp2 - 28;
 
-    if (testData[0].hcp === highestHCP && testData[0].hcp !== testData[1].hcp) {
-      testData[0].hcp = testData[0].hcp - substract;
+    if (hcp1 === highestHCP && hcp1 !== hcp2) {
+      newHcp1 = hcp1 - substract;
     }
 
-    if (testData[0].hcp === highestHCP && testData[0].hcp !== testData[1].hcp) {
-      testData[1].hcp = testData[1].hcp - substract;
+    if (hcp2 === highestHCP && hcp1 !== hcp2) {
+      newHcp2 = hcp2 - substract;
     }
+
+    return { newHcp1, newHcp2 };
   }
 
-  //SPELARE 3-4
-  if (testData[2].hcp + testData[3].hcp > 28) {
-    if (testData[2].hcp === testData[3].hcp) {
-      const difference = testData[2].hcp + testData[3].hcp - 28;
+  let { newHcp1, newHcp2 } = max28perTeam(testData[0].hcp, testData[1].hcp);
+  let { newHcp3, newHcp4 } = max28perTeam(testData[2].hcp, testData[3].hcp);
 
-      testData[2].hcp = testData[2].hcp - difference / 2;
-      testData[3].hcp = testData[3].hcp - difference / 2;
-    }
+  testData[0].hcp = newHcp1;
+  testData[1].hcp = newHcp2;
+  testData[2].hcp = newHcp3;
+  testData[3].hcp = newHcp4;
 
-    const highestHCP = Math.max(testData[2].hcp, testData[3].hcp);
-    const substract = testData[2].hcp + testData[3].hcp - 28;
+  // if (testData[0].hcp + testData[1].hcp > 28) {
+  //   if (testData[0].hcp === testData[1].hcp) {
+  //     const difference = testData[0].hcp + testData[1].hcp - 28;
+  //     testData[0].hcp = testData[0].hcp - difference / 2;
+  //     testData[1].hcp = testData[1].hcp - difference / 2;
+  //   }
 
-    if (testData[2].hcp === highestHCP && testData[2].hcp !== testData[3].hcp) {
-      testData[2].hcp = testData[2].hcp - substract;
-    }
+  //   const highestHCP = Math.max(testData[0].hcp, testData[1].hcp);
+  //   const substract = testData[0].hcp + testData[1].hcp - 28;
 
-    if (testData[3].hcp === highestHCP && testData[2].hcp !== testData[3].hcp) {
-      testData[3].hcp = testData[3].hcp - substract;
-    }
-  }
+  //   if (testData[0].hcp === highestHCP && testData[0].hcp !== testData[1].hcp) {
+  //     testData[0].hcp = testData[0].hcp - substract;
+  //   }
+
+  //   if (testData[0].hcp === highestHCP && testData[0].hcp !== testData[1].hcp) {
+  //     testData[1].hcp = testData[1].hcp - substract;
+  //   }
+  // }
+
+  // //SPELARE 3-4
+  // if (testData[2].hcp + testData[3].hcp > 28) {
+  //   if (testData[2].hcp === testData[3].hcp) {
+  //     const difference = testData[2].hcp + testData[3].hcp - 28;
+
+  //     testData[2].hcp = testData[2].hcp - difference / 2;
+  //     testData[3].hcp = testData[3].hcp - difference / 2;
+  //   }
+
+  //   const highestHCP = Math.max(testData[2].hcp, testData[3].hcp);
+  //   const substract = testData[2].hcp + testData[3].hcp - 28;
+
+  //   if (testData[2].hcp === highestHCP && testData[2].hcp !== testData[3].hcp) {
+  //     testData[2].hcp = testData[2].hcp - substract;
+  //   }
+
+  //   if (testData[3].hcp === highestHCP && testData[2].hcp !== testData[3].hcp) {
+  //     testData[3].hcp = testData[3].hcp - substract;
+  //   }
+  // }
 
   console.log(testData);
 
