ebb28f75c3f057b0fd3544c61bd050a1211bcb99
Johan <johan@colburn.se>
2021-03-08 15:38:31 +0100

    fix env

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,7 +2,5 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  VUE_APP_GIT_USERNAME: '"WF1102-091"',
-  VUE_APP_GIT_PASSWORD: '"ghj3h5"'  
+ 
 })
