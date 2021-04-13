5d8096ee11aded6202637f9caf83081e238d1e5e
Rasmus Jonsson <rasmus@colburn.se>
2021-04-13 10:31:56 +0200

    git add tests.

--- src/helpers.js
+++ src/helpers.js
@@ -1,68 +1,16 @@
 const shcp = (players) => {
   let playerData = players;
-  //console.log("shcp -> playerData", playerData)
   let slopeHandicapList = [];
-
-  //FIXAR LAG hcp PÅ MAX 28
-  function max28perTeam(hcp1, hcp2) {
-    let newHcp1 = hcp1;
-    // console.log("functionmax28perTeam -> newHcp1", newHcp1);
-    let newHcp2 = hcp2;
-    // console.log("functionmax28perTeam -> newHcp2", newHcp2);
-    // if (hcp1 + hcp2 <= 28) {
-    //   return { newHcp1, newHcp2 };
-    // }
-    // if (hcp1 === hcp2) {
-    //   const difference = hcp1 + hcp2 - 28;
-    //   newHcp1 = hcp1 - difference / 2;
-    //   newHcp2 = hcp2 - difference / 2;
-    // }
-    // const highestHCP = Math.max(hcp1, hcp2);
-    // const substract = hcp1 + hcp2 - 28;
-    // if (hcp1 === highestHCP && hcp1 !== hcp2) {
-    //   console.log("functionmax28perTeam -> hcp1", hcp1);
-    //   newHcp1 = hcp1 - substract;
-    //   console.log("functionmax28perTeam -> substract", substract);
-    //   console.log("functionmax28perTeam -> newHcp1", newHcp1);
-    // }
-    // if (hcp2 === highestHCP && hcp1 !== hcp2) {
-    //   newHcp2 = hcp2 - substract;
-    // }
-    return { newHcp1, newHcp2 };
-  }
-
-  (function () {
-    let { newHcp1, newHcp2 } = max28perTeam(
-      playerData[0].hcp,
-      playerData[1].hcp
-    );
-    playerData[0].hcp = newHcp1;
-    playerData[1].hcp = newHcp2;
-  })();
-
-  (function () {
-    let { newHcp1, newHcp2 } = max28perTeam(
-      playerData[2].hcp,
-      playerData[3].hcp
-    );
-    playerData[2].hcp = newHcp1;
-    playerData[3].hcp = newHcp2;
-  })();
 
   //slope-hcp
   playerData.forEach((player) => {
     slopeHandicapList.push(player.shcp);
   });
 
-  // let hcpSlopeReduced = slopeHandicapList.map((hcpSlope) =>  hcpSlope * 0.9);
-
   let hcpSlopeReduced = slopeHandicapList.reduce((total, amount) => {
     total.push(Math.sign(amount) === 1 ? amount * 0.9 : amount * 1.1)
     return total
   }, [])
-
-  //console.log("shcp -> hcpSlopeReduced", hcpSlopeReduced)
-
 
   let smallestHCP = 999;
   hcpSlopeReduced.forEach((hcp) => {
@@ -84,4 +32,10 @@ const shcp = (players) => {
   return { slopeHandicapList, newHcpPrel };
 };
 
-export { shcp };
+
+const calculateSlopeRating = (hcp, slopeValue, courseRating, coursePar) =>  {
+  return Math.round(hcp * (slopeValue / 113) + (courseRating - coursePar));
+}
+
+
+export { shcp, calculateSlopeRating  };
---
+++ src/helpers.test.js
@@ -0,0 +1,39 @@
+import {shcp, calculateSlopeRating} from './helpers';
+
+
+let players1 = [
+    {hcp: 10.3, shcp: 10},
+    {hcp: 4.2, shcp: 3},
+    {hcp: -0.5, shcp: -2},
+    {hcp: -0.7, shcp: -2}
+]
+
+test('Test 1 of hcp to sloped hcp', () => {
+  expect(shcp(players1)).toEqual({newHcpPrel: [11, 5, 0, 0], slopeHandicapList: [10, 3, -2, -2]});
+});
+
+
+let players2 = [
+    {hcp: 10.3, shcp: 16},
+    {hcp: 4.2, shcp: 5},
+    {hcp: 8.2, shcp: 13},
+    {hcp: 14.9, shcp: 18}
+]
+
+test('Test 2 of hcp to sloped hcp', () => {
+  expect(shcp(players2)).toEqual({newHcpPrel: [10, 0, 7, 12], slopeHandicapList: [16, 5, 13, 18]});
+});
+
+
+test('Test of calculateSlopeRating (hcp 8,2, slope 135, CR 71,0, CP 72)', () => {
+    expect(calculateSlopeRating('8.2', '135.0', '71.0', 72)).toBe(9);
+});
+
+test('Test of calculateSlopeRating (hcp 14.9, slope 126.0, CR 66.4, CP 72)', () => {
+    expect(calculateSlopeRating('14.9', '126.0', '66.4', 72)).toBe(11);
+});
+  
+test('Test of calculateSlopeRating (hcp 10,3, slope 112.0, CR 66.9, CP 71)', () => {
+    expect(calculateSlopeRating('10.3', '112.0', '66.9', 71)).toBe(6);
+});
+  
