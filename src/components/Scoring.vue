05f3cc8b2ba6d38c3e1a67cea4c42e3840a4b0c9
Johan <johan@colburn.se>
2021-04-28 15:34:24 +0200

    fullscreen modal

--- src/components/Scoring.vue
+++ src/components/Scoring.vue
@@ -397,8 +397,23 @@
 
 	//deep-selector av bootstrap-modal inom scoped styled component
 	>>> .modal-content {
-		min-height: 590px;
+		//min-height: 590px;
+		border-radius: 0;
 	}
+
+	>>> .modal-dialog {
+    max-width: 100%;
+    margin: 0;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100vh;
+    display: flex;
+    position: fixed;
+    z-index: 100000;
+}
+
 
 	h2 {
 		font-weight: bold;
