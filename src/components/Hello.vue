276cc1c629f4fd6a34cb494be980a7586c1badf1
johanstenbeck <johan@colburn.se>
2022-07-10 18:01:39 +0200

    omg 4

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -155,8 +155,8 @@
 
           <b-col class="col-12 col-md-8">
             <p v-if="closed" class="mt-3 mt-md-0">
-              Omgång 3 lottas <strong>måndagen den 27 juni</strong> och pågår
-              till <strong>söndagen den 10 juli.</strong>
+              Omgång 4 startar <strong>måndagen den 11 juli</strong> och pågår
+              till <strong>söndagen den 24 juli.</strong>
               <b-alert hidden variant="info" class="small mt-3"
                 >Lag från mer än 50% av Sveriges golfklubbar finns nu
                 representerade i tävlingen!</b-alert
