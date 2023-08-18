60d9b876563e24a0ab7e8fd6e2eb97e0faefcb9c
johanstenbeck <johan@colburn.se>
2023-08-18 21:56:52 +0200

    closed

--- src/main.js
+++ src/main.js
@@ -43,7 +43,7 @@ export const globalState = new Vue({
     admin_url: 'https://admin.matchplay.se/methods/',
     compid: 'iHv4PtxyoTHLJQSJZ', //2023 local sthlm
     prevcompid: 'hXcmBwqmfWC2tYcxR', //2022 'sFAc3dvrn2P9pXHAz',    
-    closed: false,
+    closed: true,
     showTopClubs: true //activate once teams pay
   }
 })
